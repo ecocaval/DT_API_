972038cb83a3ad97cf9d11fde1f12b2ccfa0b52a
erico cavalcanti <ecocaval@hotmail.com>
2023-04-29 20:10:42 -0300

    feat: POST /booking

---
+++ src/services/booking-service/index.ts
@@ -0,0 +1,38 @@
+import { forbiddenBookingError } from './errors';
+import bookingRepository from '@/repositories/booking-repository';
+import enrollmentRepository from '@/repositories/enrollment-repository';
+import ticketsRepository from '@/repositories/tickets-repository';
+import hotelRepository from '@/repositories/hotel-repository';
+import { notFoundError } from '@/errors';
+
+async function createBooking(roomId: number, userId: number): Promise<{ bookingId: number }> {
+  const room = await hotelRepository.findRoomById(roomId);
+
+  if (!room) {
+    throw notFoundError('Room not found!');
+  }
+
+  const booking = await bookingRepository.findBookingByRoomId(roomId);
+
+  if (booking) {
+    throw forbiddenBookingError('Booking already exists!');
+  }
+
+  const enrollment = await enrollmentRepository.findByUserId(userId);
+
+  const ticket = await ticketsRepository.findTicketByEnrollmentId(enrollment.id);
+
+  const ticketType = await ticketsRepository.findTicketTypeById(ticket.ticketTypeId);
+
+  if (ticket.status !== 'PAID' || ticketType.isRemote || !ticketType.includesHotel) {
+    throw forbiddenBookingError('Verify if ticket is paid, presencial and includes hotel!');
+  }
+
+  const bookingId = await bookingRepository.createBooking(roomId, userId);
+
+  return { bookingId };
+}
+
+const bookingService = { createBooking };
+
+export default bookingService;
